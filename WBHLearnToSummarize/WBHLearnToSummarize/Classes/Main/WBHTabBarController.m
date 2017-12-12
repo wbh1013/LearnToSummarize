8d212efd03e331b9c8120c7d047f036b99adeb69
hjy <hjy@hjydeiMac.local>
2017-12-12 12:54:03 +0800

    自定义tabbar

--- WBHLearnToSummarize/WBHLearnToSummarize/Classes/Main/WBHTabBarController.m
+++ WBHLearnToSummarize/WBHLearnToSummarize/Classes/Main/WBHTabBarController.m
@@ -7,7 +7,8 @@
 //
 
 #import "WBHTabBarController.h"
-
+#import "WBHInterfaceController.h"
+#import "WBHUtilityClassController.h"
 @interface WBHTabBarController ()
 
 @end
@@ -16,22 +17,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    //界面
+    [self addChildVC:[[WBHInterfaceController alloc]init] titleString:@"界面" selectImage:@"Interface_select" normalImage:@"Interface_nomal"];
+    [self addChildVC:[[WBHUtilityClassController alloc] init] titleString:@"工具类" selectImage:@"UtilityClass_select" normalImage:@"UtilityClass_normal"];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)addChildVC:(UIViewController *)VC  titleString:(NSString *)titleString selectImage:(NSString *)selectImage normalImage:(NSString *)normalImage {
+    VC.title = titleString;
+    VC.tabBarItem.selectedImage = [[UIImage imageNamed:selectImage]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    VC.tabBarItem.image = [[UIImage imageNamed:normalImage] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    UINavigationController * nav = [[UINavigationController alloc]initWithRootViewController:VC];
+    
+    [[UINavigationBar appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor whiteColor],NSFontAttributeName:[UIFont boldSystemFontOfSize:18]}];
+    [[UINavigationBar appearance] setBarTintColor:[UIColor hx_colorWithHexRGBAString:@"006633"]];
+    [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
+    //  关闭透明，无穿透效果
+    [[UINavigationBar appearance] setTranslucent:NO];
+    nav.jz_fullScreenInteractivePopGestureEnabled = YES;
+//    [nav.navigationBar setBackgroundImage:[KMTools createImageWithColor:ThemeColor] forBarMetrics:UIBarMetricsDefault];
+//    [nav.navigationBar setShadowImage:[KMTools createImageWithColor:ThemeColor]];
+    [self addChildViewController:nav];
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
